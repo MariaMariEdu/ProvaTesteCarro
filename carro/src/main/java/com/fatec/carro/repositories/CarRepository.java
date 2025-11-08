ce490b5ad3153ce527349b5ed7a09090def10a5e
MariMariaEdu <maric.brito15@gmail.com>
2025-11-08 14:40:04 -0300

    atualização do properties

--- carro/src/main/java/com/fatec/carro/repositories/CarRepository.java
+++ carro/src/main/java/com/fatec/carro/repositories/CarRepository.java
@@ -1,9 +1,11 @@
 package com.fatec.carro.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fatec.carro.entities.Car;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car,Long>{
     
 }
