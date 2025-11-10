ede52e382b4daaf97b59be74aa309a7fc19e3a22
MariMariaEdu <maric.brito15@gmail.com>
2025-11-10 14:27:10 -0300

    Criando o delete

--- carro/src/main/java/com/fatec/carro/controllers/CarController.java
+++ carro/src/main/java/com/fatec/carro/controllers/CarController.java
@@ -2,17 +2,25 @@ package com.fatec.carro.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fatec.carro.entities.Car;
+import com.fatec.carro.repositories.CarRepository;
 import com.fatec.carro.services.CarService;
+
+import jakarta.persistence.EntityNotFoundException;
+
+
 
 
 
 @RestController
+
+
 
 @RequestMapping("/car")
 public class CarController {
@@ -26,5 +34,22 @@ public class CarController {
       
         return ResponseEntity.ok(carService.getCarById(id));
     }
+
+    @Autowired
+    private CarRepository carRepository;
+
+
+    @DeleteMapping("/{id}")
+    public void deleteCar(@PathVariable Long id){
+        if (carRepository.existsById(id)){
+            carRepository.deleteById(id);
+        }
+
+        else{
+            throw new EntityNotFoundException("Carro com ID " + id + " não encontrado");
+        }
+    }
+
+    
 
 }
